13153cdf54ba3b8c9ecb51fa3728ee1e70881d90
mahesh <mailtomahesh81200@gmail.com>
2019-10-13 00:55:03 +0530

    bloc

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ void main() {
   runApp(
     MaterialApp(
       title: 'Bloc App',
+      theme: ThemeData(
+        primaryColor: Colors.lightBlueAccent,
+
+      ),
       home: ShowScreen(),
     ),
   );
